bbb3b78821970ec9b892a303c91fb8f9bcca63ab
YauheniRusinchuk <ruevgal@gmail.com>
2019-10-10 13:08:31 +0300

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Home from './components/Home/Home'
+
+
+function App() {
+  	return (
+    	<Fragment>
+		   	<Router>
+				<Header />
+				<Switch>
+					<Main>
+						<Route path='/' component={Home} />
+					</Main>
+				</Switch>
+			</Router>
+    	</Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Home() {
+	return (
+		<div>
+			<p>HOME PAGE</p>
+		</div>
+	)
+}
+
+export default Home;
